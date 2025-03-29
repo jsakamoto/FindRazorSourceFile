70e311161dfc95b2d54fc20c21cf4f34e84e1caa
jsakamoto <junichi.jp@gmail.com>
2025-03-29 10:53:20 +0900

    Improve: no longer need to conditional compiler directive for Blazor Wasm

---
+++ FindRazorSourceFile.WebAssembly/build/FindRazorSourceFileExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace FindRazorSourceFile.WebAssembly;
+
+internal static class FindRazorSourceFileExtensions
+{
+    public static WebAssemblyHostBuilder UseFindRazorSourceFile(this WebAssemblyHostBuilder builder)
+    {
+#if ENABLE_FIND_RAZOR_SOURCE_FILE
+        builder.RootComponents.Add<ScriptInjectorComponent>("script[src]");
+#endif
+        return builder;
+    }
+}
--- SampleSites/net8.0/Client/Program.cs
+++ SampleSites/net8.0/Client/Program.cs
@@ -1,18 +1,15 @@
-﻿#if DEBUG
-using FindRazorSourceFile.WebAssembly;
-#endif
+﻿using FindRazorSourceFile.WebAssembly;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SampleSite.Client.Services;
 using SampleSite.Components;
 using SampleSite.Components.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-#if DEBUG
-builder.UseFindRazorSourceFile();
-#endif
 builder.RootComponents.Add<App>("app");
-builder.Services
-    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
-    .AddScoped<IWeatherForecastService, WeatherForecastService>();
+
+builder.UseFindRazorSourceFile();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
 
 await builder.Build().RunAsync();
